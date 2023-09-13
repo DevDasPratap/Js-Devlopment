c933479dadb2145a22d209c5b5bd94633c8ab693
DevDas <epratapdas@gmail.com>
2023-09-13 20:33:13 +0530

    added object question answser

---
+++ JS/object-qna.js
@@ -0,0 +1,10 @@
+// Copy object
+// const orginal = {name:'Pratap', pass:1234}
+// const copy = {...orginal}
+// console.log(copy)
+
+// merge object
+// const obj1 = {name:'Pratap', pass:1234}
+// const obj2 = {name:'Das', pass:98710, isActive:true}
+// const merge = {...obj1, ...obj2}
+// console.log(merge)
