163230c20d0077135b802da23a9a42d5058f8e89
Pratap Das <pratapdas@Prataps-MacBook-Air.local>
2023-09-23 12:56:07 +0530

    Nodejs practice

--- NodeJS/Http/http.js
+++ NodeJS/Http/http.js
@@ -1,3 +1,25 @@
+/*
+It is a protocol used to transmitting data over the internet,
+HTTP defines how clients and servers communicate with each other
+and how req, res should be formatted and transmitted.
+
+http module- Node http is an intregral component that
+facilitates various aspects of the http protocol
+
+*/
+
 const http = require('http')
 // console.log(http)
 // console.log(http.STATUS_CODES)
+
+const server = http.createServer((req, res)=>{
+    if (req.url == '/') {
+        res.write(`<h1>Home</h1>`)
+    }else if (req.url == '/about') {
+        res.write(`<p>About</p>`)
+    }
+    res.end()
+})
+
+server.listen(5001)
+console.log('The server is running')
