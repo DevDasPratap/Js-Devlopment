9b1947da8482e7a9048dfaf580bc393757627c75
devdaspratap <contact.pratap.das@gmail.com>
2023-11-27 23:08:29 +0530

    models added

---
+++ JS/DSA/remove_duplicate.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+ */
+const arr = [0,0,1,1,1,2,2,3,3,4]
+const target = 5
+function remove_duplicate_sorted_array(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arr.splice(1)
+            }
+        }
+    }
+}
+const res = remove_duplicate_sorted_array(arr, target)
