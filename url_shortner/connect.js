802c65af6fd67e22c5863eb1484afc1513d91672
devdaspratap <contact.pratap.das@gmail.com>
2024-01-17 20:18:36 +0530

    remove

--- url_shortner/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', true)
-async function connectToDB(url) {
-    return await mongoose.connect(url)
-}
-
-module.exports = connectToDB
--- url_shortner/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { user_signup, user_login } = require('../controllers/user')
-
-const router = express.Router()
-
-router.post('/', user_signup)
-router.post('/login', user_login)
-
-module.exports = router
