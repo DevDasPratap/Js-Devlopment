353ef7a7b35102c94f39ba04dc785d11ee4f77a0
devdaspratap <contact.pratap.das@gmail.com>
2024-01-18 21:54:35 +0530

    backend in js practice

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
-const http = require('http')
-const app = require('./app/app')
+// require('dotenv').config()
+// const http = require('http')
+// const app = require('./app/app')
 
-const server = http.createServer(app)
-const port = process.env.PORT || 5555
-server.listen(port, ()=>{
-    console.log(`Server running on port ${port}`)
-})
+// const server = http.createServer(app)
+// const port = process.env.PORT || 5555
+// server.listen(port, ()=>{
+//     console.log(`Server running on port ${port}`)
+// })
