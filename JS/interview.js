19bec915336e4ca2926e2ca2f810004d08b5279a
devdaspratap <contact.pratap.das@gmail.com>
2024-03-26 07:49:55 +0530

    SQL practice

---
+++ JS/interview.js
@@ -0,0 +1,26 @@
+/**
+ * 🚀 Master Your Front-End coding Interview with these 20 JavaScript Questions! 🚀
+
+1. write a function that can give you unique character string from string.
+2. write a function that takes an array of integers and return maximum product that can be obtained by multiplying any three from that array.
+3. write a function to move all zero to the end of the array while maintaining the relative order of the non-zero elements.
+4. write a function to check palindrome, considering only alphanumeric characters and ignoring cases.
+5. given a sorted array, and target value, write a function to return the index of the target value in the array. if target not found- return -1
+
+6. write a function which return first non-repeating character, if not available then return -1
+7. write a function to find occurrence of characters in the given string
+8. Flatten a nested array
+9. Find the longest word in a string
+10. Compare two arrays
+
+11. write a function that accepts an unlimited number of inputs and returns the sum of them.
+12. Write a program to remove duplicate element from an array in different ways.
+13. Write a program to implement currying which gives the sum of three numbers.
+14. Write a program to generate a 6-digit random OTP.
+15. Write a program to find the intersection of two arrays.
+16. Write a JavaScript program for custom array method for max and min value.
+17. Write a JavaScript program to capitalize the first letter of each word of a given string.
+18. Write a JavaScript program that determines whether a given string contains only unique characters or not.
+19. Write a JavaScript function that checks if the sum of all integers in the array equals the highest integer in that array.
+20. Write a JavaScript program to find unique object property values.
+ */
