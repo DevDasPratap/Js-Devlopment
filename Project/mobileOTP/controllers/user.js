b9f803388a11028a2f5fe69ab5db4560e52ad78d
devdaspratap <contact.pratap.das@gmail.com>
2024-08-06 19:53:58 +0530

    nodejs backend mobile otp practice

--- Project/mobileOTP/controllers/user.js
+++ Project/mobileOTP/controllers/user.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const OTPGenerator = require('otp-generator')
 const tiwilo = require('twilio')
 const otpModel = require('../models/otp')
-const twilioAccountSID = process.env.TWILIO_ACCOUNT_SID || 'ACd06a94fb0dd55e1fca8ee1db7dd909db'
-const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN || '9c88585a46033d562a2bbd8b45e769d7'
-const twilioPhoneNumber =  process.env.TWILIO_PHONE_NUMBER || '+12135831310'
+const twilioAccountSID = process.env.TWILIO_ACCOUNT_SID
+const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN
+const twilioPhoneNumber =  process.env.TWILIO_PHONE_NUMBER
 
 if (!twilioAccountSID || !twilioAuthToken || !twilioAuthToken) {
     console.error("Twilio environment variables are not set correctly.");
