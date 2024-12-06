db095a61092624f5e98a8758dce5f436d1eef77f
devdaspratap <contact.pratap.das@gmail.com>
2024-12-06 23:52:05 +0530

    search function

--- FullStack/Frontend/stateLifting/src/components/Table.jsx
+++ FullStack/Frontend/stateLifting/src/components/Table.jsx
@@ -1,48 +1,58 @@
 import { useState } from "react";
 
 const Table = ({ contacts }) => {
-    const [filter, setFilter] = useState('')
+    const [filter, setFilter] = useState('all')
 
-    const filterHandler = (e)=>{
+    const filterHandler = (e) => {
         setFilter(e.target.value)
     }
 
+    const [search, setSearch] = useState('')
     let filterContact = []
     if (filter === 'all') {
         filterContact = contacts
-    }else{
+    } else {
         filterContact = contacts.filter(contact => contact.address === filter)
     }
+
+    if (search) {
+        filterContact = filterContact.filter(
+            (contact) =>
+                contact.name.includes(search) || contact.email.includes(search)
+        );
+    }
+
     return (
         <>
-        <div>
-            <br/>
-            Filter:
-            <select value={filter} onChange={filterHandler}>
-                <option value='all'>All</option>
-                <option value=''>None</option>
-                <option value='home' >Home</option>
-                <option value='office'>Office</option>
-            </select>
-        </div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                </tr>
-            </thead>
-            <tbody>
-                {filterContact.map((contact, index) => (
-                    <tr key={index}>
-                        <td>{contact.name}</td>
-                        <td>{contact.email}</td>
-                        <td>{contact.address}</td>
+            <div>
+                <br />
+                Filter:
+                <select value={filter} onChange={filterHandler}>
+                    <option value='all'>All</option>
+                    <option value=''>None</option>
+                    <option value='home' >Home</option>
+                    <option value='office'>Office</option>
+                </select>
+                <input type="search" placeholder="Type here" value={search} onChange={(e) => setSearch(e.target.value)} />
+            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Address</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {filterContact.map((contact, index) => (
+                        <tr key={index}>
+                            <td>{contact.name}</td>
+                            <td>{contact.email}</td>
+                            <td>{contact.address}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 };
