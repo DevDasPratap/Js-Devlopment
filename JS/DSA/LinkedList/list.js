88863b32b8893bf0329974955899b174a6522554
devdaspratap <contact.pratap.das@gmail.com>
2025-04-09 23:18:36 +0530

    linkedlist concept clear

--- JS/DSA/LinkedList/list.js
+++ JS/DSA/LinkedList/list.js
@@ -1,7 +1,106 @@
-function node(data) {
-    this.data = data
-    this.next = null
+/**
+ * Head pointer will always be the first of the linked list
+ * Current pointer will be used for traversing
+ */
+
+
+class Node {
+    constructor(data) {
+        // One node is an object having 2 part (data/referance other node)
+        this.data = data // Store data in the node
+        this.next = null;  // Pointer to the next node, initially set to null
+    }
 }
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+    // Append operation - adds a new node to the end of the list
+    append(data){
+        const newNode = new Node(data)
+        if (this.head===null) {
+            this.head=newNode
+            this.head.next=null
+        }else{
+            // add at last position if head not null
+            let current = this.head
+            while (current.next !== null) {
+                current = current.next
+            }
+            current.next = newNode
+        }
+    }
+    // Display operation - prints the linked list
+    display(){
+        let current = this.head
+        while (current) {
+            // console.log(current)
+            // console.log(current.data)
+            console.log(`Current: ${current.data} - Next: ${current.next ? current.next.data : null}`)
+            current = current.next
+        }
+    }
+    // Prepend operation - adds a new node to the beginning of the list
+    prepend(data){
+        const newNode = new Node(data)
+        newNode.next = this.head // point new node to current head
+        this.head = newNode // update head to new node
+    }
+    // get current head
+    getCurrentHead(){
+        return this.head
+    }
+
+    // get current tail
+    getCurrentTail(){
+        let tail = this.head
+        while (tail && tail.next !== null) {
+            tail = tail.next
+        }
+        return tail
+    }
+
+    // insert at a particular position
+    insertAtPosition(target, data){
+        const newNode = new Node(data)
+        let current = this.head
+        while (current !== null) {
+            if (current.data === target) {
+                newNode.next = current.next
+                current.next = newNode
+            }
+            current = current.next
+            if (current === null) {
+                console.log('Target not found')
+                return
+            }
+        }
+    }
+}
+
+const linklist = new LinkedList()
+linklist.append(1)
+linklist.append(2)
+linklist.append(3)
+linklist.append(4)
+linklist.append(5)
+// linklist.display()
+linklist.prepend(0)
+linklist.display()
+// const currentHead = linklist.getCurrentHead()
+// console.log('Current Head:', currentHead)
+// const currentTail = linklist.getCurrentTail()
+// console.log('Current Tail:', currentTail)
+
+linklist.insertAtPosition(3, 3.5)
+linklist.display()
+
+
+// function node(data) {
+//     this.data = data
+//     this.next = null
+// }
 
 // const head = new node(1)
 // // console.log('head',head)
@@ -52,75 +151,75 @@ function node(data) {
 
 
 // Define a node constructor function
-function Node(data) {
-    this.data = data;  // Store data in the node
-    this.next = null;  // Pointer to the next node, initially set to null
-}
+// function Node(data) {
+//     this.data = data;  // Store data in the node
+//     this.next = null;  // Pointer to the next node, initially set to null
+// }
 
-// Creating the linked list
-let head = new Node(1);  // Create the first node with value 1
-let mid = new Node(5);   // Create the second node with value 5
-head.next = mid;         // Link first node to second node
+// // Creating the linked list
+// let head = new Node(1);  // Create the first node with value 1
+// let mid = new Node(5);   // Create the second node with value 5
+// head.next = mid;         // Link first node to second node
 
-// Insert at the beginning (start)
-let newnode = new Node(10);  // Create a new node with value 10
-newnode.next = head;         // Point new node to the current head
-head = newnode;              // Update head to the new node
-// console.log('After inserting at start:', head);
+// // Insert at the beginning (start)
+// let newnode = new Node(10);  // Create a new node with value 10
+// newnode.next = head;         // Point new node to the current head
+// head = newnode;              // Update head to the new node
+// // console.log('After inserting at start:', head);
 
-// Insert at the end
-function insertAtEnd(head, value) {
-    let newNode = new Node(value);  // Create a new node with given value
-    if (head === null) {
-        head = newNode;             // If list is empty, new node becomes head
-        return head;
-    }
-    let temp = head;
-    while (temp.next !== null) {    // Traverse to the last node
-        temp = temp.next;
-    }
-    temp.next = newNode;             // Set last node's next to new node
-    return head;
-}
-head = insertAtEnd(head, 20);
-// console.log('After inserting at end:', head);
+// // Insert at the end
+// function insertAtEnd(head, value) {
+//     let newNode = new Node(value);  // Create a new node with given value
+//     if (head === null) {
+//         head = newNode;             // If list is empty, new node becomes head
+//         return head;
+//     }
+//     let temp = head;
+//     while (temp.next !== null) {    // Traverse to the last node
+//         temp = temp.next;
+//     }
+//     temp.next = newNode;             // Set last node's next to new node
+//     return head;
+// }
+// head = insertAtEnd(head, 20);
+// // console.log('After inserting at end:', head);
 
-// Insert at a particular index
-function insertAtIndex(head, index, value) {
-    let newNode = new Node(value);
-    if (index === 0) {
-        newNode.next = head;          // Insert at the start
-        head = newNode;
-        return head;
-    }
-    let temp = head;
-    let count = 0;
-    while (temp !== null && count < index - 1) {  // Traverse to the previous node
-        temp = temp.next;
-        count++;
-    }
-    if (temp === null) {
-        // console.log('Index out of bounds');
-        return head;
-    }
-    newNode.next = temp.next;  // Point new node to next of temp
-    temp.next = newNode;       // Insert new node after temp
-    return head;
-}
-head = insertAtIndex(head, 1, 15);
-// console.log('After inserting at index 1:', head);
+// // Insert at a particular index
+// function insertAtIndex(head, index, value) {
+//     let newNode = new Node(value);
+//     if (index === 0) {
+//         newNode.next = head;          // Insert at the start
+//         head = newNode;
+//         return head;
+//     }
+//     let temp = head;
+//     let count = 0;
+//     while (temp !== null && count < index - 1) {  // Traverse to the previous node
+//         temp = temp.next;
+//         count++;
+//     }
+//     if (temp === null) {
+//         // console.log('Index out of bounds');
+//         return head;
+//     }
+//     newNode.next = temp.next;  // Point new node to next of temp
+//     temp.next = newNode;       // Insert new node after temp
+//     return head;
+// }
+// head = insertAtIndex(head, 1, 15);
+// // console.log('After inserting at index 1:', head);
 
-// Function to print the linked list
-function printList(head) {
-    let temp = head;
-    while (temp !== null) {
-        // console.log(temp.data);
-        temp = temp.next;
-    }
-}
+// // Function to print the linked list
+// function printList(head) {
+//     let temp = head;
+//     while (temp !== null) {
+//         // console.log(temp.data);
+//         temp = temp.next;
+//     }
+// }
 
-// console.log('Final Linked List:');
-printList(head);
+// // console.log('Final Linked List:');
+// printList(head);
 
 
 
