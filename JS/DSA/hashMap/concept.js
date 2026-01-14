f3be68bcb887666efc334816315b8ae2ccd8065e
devdaspratap <contact.pratap.das@gmail.com>
2026-01-14 12:33:22 +0530

    validator add

---
+++ JS/DSA/hashMap/concept.js
@@ -0,0 +1,15 @@
+/**
+ * Application of Hashing
+ *  - Dictionaries
+ *  - Database indexing
+ *  - Cryptography
+ *  - Cache
+ *  - Symbol Tables in compiler/intepreters
+ *  - Routes
+ *  - Getting data from database
+ *  - Many more
+ */
+/**
+ * Direct Address Table
+ * Hashing Functions
+ */
