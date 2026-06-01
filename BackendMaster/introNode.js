f05e7b25b28b4684257df1df45280572ad89bd09
devdaspratap <contact.pratap.das@gmail.com>
2026-06-01 22:53:55 +0530

    intro of node.js

---
+++ BackendMaster/introNode.js
@@ -0,0 +1,42 @@
+/**
+ * What is node.js
+ * what it is not?
+ * internal of node.js
+ * how nodejs code is written
+ * chrome v8 engine
+ * internal of v8 engine
+ * 
+ * what is run time environment?
+ *  => it any technology not sufficient engouth and you to add more capablity corospoding technogy top of it
+ *  runtime env also a software that can provide all the relavent extra eco system stuff that the technology that does'n have inheritly
+ *  here we talkin about javascrpit 
+ * 
+ * js is a programmming language have lot of limitation
+ * previously js not powerful, you cant create end to end client backend script build now days we do end to end with js
+ * now runtime env provide extra capablity to javascript
+ * here is runtime env for js: setTimeout, setInterval, console.log()
+ * runtime know how to access timers, console etc
+ * runtime env is: browser, node.js etc
+ * browser runtime have access timers, reading, writing/updating etc html operation, network call like fetch function
+ * node.js runtime env OS related capability to js it diffrent from browser runtime env, so now day we run js on our machine
+ * node.js not frame work, it is sowftware that provide extra cability to js
+ * 
+ * cross platform: windows and macos are completely diffrent system
+ * if we run c++ code on window we make sure ut .exe executable file otherwise not possible to run. c++ code -> compile -> .exe file then we run on window but we cant run on diffrent os system like macos
+ * 
+ * 
+ * internal of node.js
+ * node.js provide lots of feature provide access
+ * like: v8 engine, libuv, event loop, os related (http, fs, net, os, etc)
+ * 
+ * so how js code intract with node.js?
+ * 
+ * node.js ecosystem have internally all of things -> v8 engine, libuv, event loop, os related (http, fs, net, os, crypto etc)
+ * but on the out side we write some js code like timers setTimeout
+ * node.js expose set of function then we can call them in the js layer of node.js
+ * in js layer some time call direct with node.js or some time call through c++ layer
+ * 
+ * 
+ * 
+ */
+
